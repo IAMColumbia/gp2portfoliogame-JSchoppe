83d3fe9f40b773a1dd8e012384693444516235ca
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-03-16 14:08:00 -0500

    adds some common utilities
    adds some core utilities to help abstract away from MonoBehaviour

---
+++ src/Brute Drive/Assets/Scripts/EngineCore/IEditorWrapper.cs
@@ -0,0 +1,18 @@
+﻿namespace BruteDrive.EngineCore
+{
+    /// <summary>
+    /// Implemented by classes that wrap simpler classes.
+    /// to be shown in an editor.
+    /// </summary>
+    /// <typeparam name="T">The simple class or struct being wrapped.</typeparam>
+    public interface IEditorWrapper<T>
+    {
+        #region Method Requirements
+        /// <summary>
+        /// Gets the underlying class without editor context.
+        /// </summary>
+        /// <returns>The wrapped object.</returns>
+        T Instance();
+        #endregion
+    }
+}
