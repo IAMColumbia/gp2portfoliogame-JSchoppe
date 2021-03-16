5f84b471248b10ebf8a70f16de368ebef479d07a
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-03-16 14:08:00 -0500

    adds some common utilities
    adds some core utilities to help abstract away from MonoBehaviour

---
+++ src/Brute Drive/Assets/Scripts/EngineCore/ITickProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace BruteDrive.EngineCore
+{
+    #region Event Delegates
+    /// <summary>
+    /// Function that responds to Ticking routines.
+    /// </summary>
+    /// <param name="deltaTime">The change in time in seconds.</param>
+    public delegate void TickListener(float deltaTime);
+    #endregion
+    /// <summary>
+    /// Implements tick events that pass time deltas.
+    /// </summary>
+    public interface ITickProvider
+    {
+        #region Event Requirements
+        /// <summary>
+        /// Called on tick as specified by the provider.
+        /// Passes change in time since last tick.
+        /// </summary>
+        event TickListener Tick;
+        #endregion
+    }
+}
