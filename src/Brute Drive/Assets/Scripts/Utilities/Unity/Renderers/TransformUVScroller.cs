39341e26455bda9c702fdf59114626ef125be595
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-03-29 11:49:08 -0500

    imports common utility scripts for unity
    imports some reusable engine abstraction and extension utilities

---
+++ src/Brute Drive/Assets/Scripts/Utilities/Unity/Renderers/TransformUVScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformUVScroller : MonoBehaviour
+{
+    [SerializeField] private Transform drivingTransform;
+    [SerializeField] private Material targetMaterial;
+    [SerializeField] private float scale = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        targetMaterial.mainTextureOffset = new Vector2(-drivingTransform.position.x * scale, -drivingTransform.position.z * scale);
+    }
+}
