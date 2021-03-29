9f803fb4ce69fc2ee4f6983aa78badfd12ca26b7
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-03-29 11:47:12 -0500

    adds primitive vehicle implementation

---
+++ src/Brute Drive/Assets/Scripts/Vehicles/IVehicleRenderer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine; // Only used for Vector2.
+
+namespace BruteDrive.Vehicles
+{
+    /// <summary>
+    /// Implements the rendered elements of the vehicle.
+    /// </summary>
+    public interface IVehicleRenderer
+    {
+        #region Property Requirements
+        /// <summary>
+        /// Updates the position of the rendered vehicle.
+        /// </summary>
+        Vector2 Position { set; }
+        /// <summary>
+        /// Updates the direction of the rendered vehicle.
+        /// </summary>
+        Vector2 Forwards { set; }
+        /// <summary>
+        /// Updates the steer angle of the rendered vehicle.
+        /// </summary>
+        float SteerAngle { set; }
+        #endregion
+    }
+}
