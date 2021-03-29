70f07cbc98564731aa995c5191ca771c85ed46d9
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-03-29 11:54:26 -0500

    first pass route generation
    implements a basic procedure for generating a map

---
+++ src/Brute Drive/Assets/Scripts/StageGeneration/MapsIterator.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using Google.Maps;
+using Google.Maps.Coord;
+using Google.Maps.Event;
+using Google.Maps.Feature.Style;
+using Google.Maps.Unity.Intersections;
+
+namespace BruteDrive.StageGeneration
+{
+    /// <summary>
+    /// Iteratively generates a map
+    /// </summary>
+    public sealed class MapsIterator : MonoBehaviour
+    {
+        [Tooltip("The service provider for the maps API.")]
+        [SerializeField] private MapsService mapsService = default;
+
+        [Tooltip("The coordinates of the location to load.")]
+        [SerializeField] private LatLng coordinates = default;
+
+        [SerializeField] private Material filler = default;
+        [SerializeField] private Material other = default;
+
+        [SerializeField] private int desiredNodes = 300;
+        [SerializeField] private float scanStep = 50f;
+        [SerializeField] private float maxScan = 1000f;
+
+
+
+
+        private float searchScale;
+
+        private IGeneratorListener listener;
+
+        private MapLoadRegion region;
+
+        private GameObjectOptions options;
+
+        public void TryGenerate(IGeneratorListener callbackContext)
+        {
+            listener = callbackContext;
+
+            // This initializes the maps service
+            // with the given coordinates.
+            mapsService.InitFloatingOrigin(coordinates);
+
+            // Listen for the state changes in the map loader.
+            mapsService.Events.MapEvents.Loaded.AddListener(OnLoaded);
+            mapsService.Events.MapEvents.LoadError.AddListener(OnFailed);
+
+            mapsService.Events.RegionEvents.WillCreate.AddListener(
+                (WillCreateRegionArgs args) => { args.Cancel = true; });
+            /*
+            mapsService.Events.ModeledStructureEvents.WillCreate.AddListener(
+                (WillCreateModeledStructureArgs args) => { args.Cancel = true; });
+            */
+
+            searchScale = scanStep;
+
+            options = new GameObjectOptions()
+            {
+                ExtrudedStructureStyle = new ExtrudedStructureStyle.Builder
+                {
+                    WallMaterial = filler,
+                    RoofMaterial = filler
+                }.Build(),
+                ModeledStructureStyle = new ModeledStructureStyle.Builder
+                {
+                    Material = filler
+                }.Build(),
+                RegionStyle = new RegionStyle.Builder
+                {
+                    FillMaterial = other
+                }.Build(),
+                AreaWaterStyle = new AreaWaterStyle.Builder
+                {
+                    FillMaterial = filler
+                }.Build(),
+                LineWaterStyle = new LineWaterStyle.Builder
+                {
+                    Material = filler
+                }.Build(),
+                SegmentStyle = new SegmentStyle.Builder
+                {
+                    Material = filler,
+                    IntersectionMaterial = filler,
+                    Width = 7.0f
+                }.Build(),
+            };
+
+
+            region = mapsService.MakeMapLoadRegion()
+              .AddCircle(Vector3.zero, searchScale).Load(options);
+        }
+
+        private void OnLoaded(MapLoadedArgs args)
+        {
+            int count = 0;
+            foreach (RoadLatticeNode node in mapsService.RoadLattice.Nodes)
+                count++;
+
+
+            if (count > desiredNodes || searchScale > maxScan)
+                listener.OnLoaded();
+            else
+            {
+                searchScale += scanStep;
+                region.AddCircle(Vector3.zero, searchScale).Load(options);
+            }
+        }
+        private void OnFailed(MapLoadErrorArgs args)
+        {
+            listener.OnFailed();
+        }
+    }
+}
---
+++ src/Brute Drive/Assets/Scripts/StageGeneration/RouteGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Maps;
+using Google.Maps.Coord;
+using Google.Maps.Event;
+using Google.Maps.Feature.Style;
+using Google.Maps.Unity.Intersections;
+
+namespace BruteDrive.StageGeneration
+{
+    public sealed class RouteGenerator : MonoBehaviour
+    {
+
+        [Tooltip("The service provider for the maps API.")]
+        [SerializeField] private MapsService mapsService = default;
+
+        [SerializeField] private float desiredDistance = 300f;
+
+        public RoadLatticeNode[] GenerateRoute()
+        {
+            int count = 0;
+            foreach (RoadLatticeNode node in mapsService.RoadLattice.Nodes)
+                count++;
+
+            // Initialize collections for graph exploration.
+            RoadLatticeNode[] bestPath = default;
+            float bestPathLength = 0f;
+            Stack<RoadLatticeNode> currentPath = new Stack<RoadLatticeNode>();
+            float currentPathLength = 0f;
+            // Initialize starting point for route finding.
+            RoadLatticeNode current = mapsService.RoadLattice.SnapToNode(Vector2.zero);
+            currentPath.Push(current);
+            // Kick off recursive algorithm to find
+            // a suitable path for the desired distance.
+            ExploreJunctionsRecursive(current);
+            return bestPath;
+
+            bool ExploreJunctionsRecursive(RoadLatticeNode currentNode)
+            {
+                foreach (RoadLatticeNode node in currentNode.Neighbors)
+                {
+                    if (!currentPath.Contains(node))
+                    {
+                        // This path is not explored.
+                        currentPath.Push(node);
+                        float deltaLength = Vector2.Distance(currentNode.Location, node.Location);
+                        currentPathLength += deltaLength;
+                        // Will this path be a new record?
+                        if (currentPathLength > bestPathLength)
+                        {
+                            // Document the new record.
+                            bestPathLength = currentPathLength;
+                            bestPath = currentPath.ToArray();
+                            // If this path is long enough,
+                            // then complete the search.
+                            if (bestPathLength > desiredDistance)
+                                return true;
+                        }
+                        // Keep exploring down this node.
+                        if (ExploreJunctionsRecursive(node))
+                            // Did somewhere deeper find a solution?
+                            // If so bubble out of recursion.
+                            return true;
+                        // Finish exploring this branch.
+                        currentPathLength -= deltaLength;
+                        currentPath.Pop();
+                    }
+                }
+                // Nothing down this path yielded
+                // the desired path length.
+                return false;
+            }
+        }
+    }
+}
