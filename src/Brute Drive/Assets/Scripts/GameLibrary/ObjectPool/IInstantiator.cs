3e2939b9de0b791ee1194ffd23fffd47d2067843
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-04-19 11:21:18 -0500

    implements generic object pool
    abstracts object pool and creates a Unity wrapped instance that can use a prefab

---
+++ src/Brute Drive/Assets/Scripts/GameLibrary/ObjectPool/IInstantiator.cs
@@ -0,0 +1,17 @@
+﻿namespace GameLibrary.ObjectPool
+{
+    /// <summary>
+    /// Implements an instantiator for an object type.
+    /// </summary>
+    /// <typeparam name="T">The object that will be instantiated.</typeparam>
+    public interface IInstantiator<T>
+    {
+        #region Methods Implemented
+        /// <summary>
+        /// Makes an instantiated copy of the object.
+        /// </summary>
+        /// <returns>A new object instance.</returns>
+        public T Instantiate();
+        #endregion
+    }
+}
